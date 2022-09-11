44a3c0403e5993b48e276dd4a1c89b6a80949f63
Joelhiguera <103140051+Joelhiguera@users.noreply.github.com>
2022-09-10 22:39:57 -0600

    complete App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Page from './components/Page'
+import Header from './components/Header'
+import Aboutme from './components/Aboutme';
+import Footer from './components/Footer'
+import Contact from './components/Contact';
+import Navigator from './components/Navigator';
 
 function App() {
+  const [pages] = useState([
+    {name: 'About Me'},
+    {name: 'Portfolio'},
+    {name: 'Contact'}, 
+    {name: 'Resume'},
+  ])
+  const [currentPage, setCurrentPage] = useState(pages[0])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header>
+        <Navigator 
+          pages={pages}
+          setCurrentPage={setCurrentPage}
+          currentPage={currentPage}
+          >
+        </Navigator>
+        </Header>
+        <main>
+          <Page currentPage={currentPage}></Page>
+        </main>
+
+      <Footer />
+      
+    </>
   );
 }
 
