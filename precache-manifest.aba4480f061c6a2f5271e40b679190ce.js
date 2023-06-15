540110e216b434acfed98b61a13efa416915099d
Joelhiguera <103140051+Joelhiguera@users.noreply.github.com>
2023-06-14 19:06:51 -0600

    Updates

---
+++ precache-manifest.aba4480f061c6a2f5271e40b679190ce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "89458a8cfa6037b4b1f1",
+    "url": "/./static/css/main.408016e7.chunk.css"
+  },
+  {
+    "revision": "89458a8cfa6037b4b1f1",
+    "url": "/./static/js/main.89458a8c.chunk.js"
+  },
+  {
+    "revision": "19d0d91d151bb888dfcf",
+    "url": "/./static/js/1.19d0d91d.chunk.js"
+  },
+  {
+    "revision": "8c91a21b86f41b9c9369",
+    "url": "/./static/js/runtime~main.8c91a21b.js"
+  },
+  {
+    "revision": "64b069464ed6a41055a8772598af2777",
+    "url": "/./static/media/avatar.64b06946.jpg"
+  },
+  {
+    "revision": "f5497806aa6276ed3d75c07e84d91845",
+    "url": "/./static/media/Resume2023.f5497806.pdf"
+  },
+  {
+    "revision": "40480605dda6f9030ef775ef5b40bfc1",
+    "url": "/./index.html"
+  }
+];
