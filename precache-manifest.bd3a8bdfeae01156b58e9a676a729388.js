96d12144e2261d217a56de8fd4d322bdc1fc1317
Joelhiguera <103140051+Joelhiguera@users.noreply.github.com>
2023-06-14 19:53:43 -0600

    Updates

---
+++ precache-manifest.bd3a8bdfeae01156b58e9a676a729388.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "89458a8cfa6037b4b1f1",
+    "url": "/react-portfolio/static/css/main.408016e7.chunk.css"
+  },
+  {
+    "revision": "89458a8cfa6037b4b1f1",
+    "url": "/react-portfolio/static/js/main.89458a8c.chunk.js"
+  },
+  {
+    "revision": "19d0d91d151bb888dfcf",
+    "url": "/react-portfolio/static/js/1.19d0d91d.chunk.js"
+  },
+  {
+    "revision": "f8926043fa4459ec0101",
+    "url": "/react-portfolio/static/js/runtime~main.f8926043.js"
+  },
+  {
+    "revision": "64b069464ed6a41055a8772598af2777",
+    "url": "/react-portfolio/static/media/avatar.64b06946.jpg"
+  },
+  {
+    "revision": "f5497806aa6276ed3d75c07e84d91845",
+    "url": "/react-portfolio/static/media/Resume2023.f5497806.pdf"
+  },
+  {
+    "revision": "2fe1421e29e605f06ff1b67b52ded4f3",
+    "url": "/react-portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio./precache-manifest.fbdac8a81ac7f68b4e2bd8018a3ae4d5.js"
+  "/react-portfolio/precache-manifest.bd3a8bdfeae01156b58e9a676a729388.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-portfolio./index.html", {
+workbox.routing.registerNavigationRoute("/react-portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
